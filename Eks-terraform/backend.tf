4de0c110f7b2be83b9b34df5513bb842721a5091
CHRAJKUMAR22 <97086614+CHRAJKUMAR22@users.noreply.github.com>
2026-05-22 06:57:03 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,9 +1,12 @@
 terraform {
-  backend "s3" {
-    bucket         = "raju-mrcloudbook777"
-    key            = "EKS/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
